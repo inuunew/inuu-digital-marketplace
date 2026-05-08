fa402fa855fe2bff89e220c004c9b306f6d5c56c
inuunew <choirulazzamibnu@gmail.com>
2026-05-08 14:08:38 +0700

    Delete app.js

--- js/app.js
+++
@@ -1,9 +0,0 @@
-
-function navigate(page){
-  document.getElementById("app").innerHTML = pages[page] || "<h2>404</h2>";
-}
-window.navigate = navigate;
-
-document.addEventListener("DOMContentLoaded", ()=>{
-  navigate("home");
-});
